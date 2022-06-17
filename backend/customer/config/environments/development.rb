0202cdaf61cead4b36a69d8e019feb9f1070761e
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-06-18 04:54:31 +0800

    working api query locally, but failed to migrate to cloud

--- backend/customer/config/environments/development.rb
+++ backend/customer/config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.hosts << "dbs-service-qz6esvmepq-as.a.run.app"
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
