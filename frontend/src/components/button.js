b6e533b59dc92ef2be64f3297f0bfe83ff63fb67
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-06-17 17:30:59 +0800

    first proper commit with ruby on rails backend

--- frontend/src/components/button.js
+++
@@ -1,8 +0,0 @@
-export default function Button({text, bgcolor, hovercolor, onClick}) {
-  return (
-    <button className = {`${bgcolor} ${hovercolor} text-white font-bold py-2 px-4 rounded w-full`} onClick={onClick}>
-      {text}
-    </button>
-  );
-}
-
