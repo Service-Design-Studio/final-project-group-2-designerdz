8e4bce87dc2fb73272f5fa833aa0086bac0a5f0b
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-06-17 17:32:06 +0800

    first proper commit

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,8 @@
+export default function Button({text, bgcolor, hovercolor, onClick}) {
+  return (
+    <button className = {`${bgcolor} ${hovercolor} text-white font-bold py-2 px-4 rounded w-full`} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
---
+++ frontend/src/pages/Details.js
@@ -0,0 +1,17 @@
+import Button from "../components/Button.js";
+import { useNavigate } from "react-router-dom";
+
+export default function Details() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Details</h1>
+      <Button
+        text="NEXT"
+        bgcolor="bg-green-500"
+        hovercolor="hover:bg-cyan-500"
+        onClick={() => navigate("/passport")}
+      />
+    </div>
+  );
+}
