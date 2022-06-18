140fddafa340e04b6129dd36d487f64dbeb36a4d
Jolow <jolow999@gmail.com>
2022-06-18 09:54:32 +0800

    Added form elements for passport.js TODO: How to pass data from 1 page to another? Or will we be using the database for that?

--- frontend/src/pages/Passport.js
+++ frontend/src/pages/Passport.js
@@ -1,17 +1,28 @@
 import Button from "../components/Buttons.js";
 import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 export default function Passport() {
   const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    navigate("/review");
+  }
+  console.log(errors)
+
   return (
-    <div>
-      <h1>Passport</h1>
-      <Button
-        text="NEXT"
-        bgcolor="bg-green-500"
-        hovercolor="hover:bg-cyan-500"
-        onClick={() => navigate("/review")}
-      />
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col ">
+      <input type="text" placeholder="Full Name" {...register("Full Name", {required: true})} />
+      <input type="text" placeholder="Passport Number" {...register("Passport Number", {required: true})} />
+      <input type="datetime" placeholder="Passport Expiry" {...register("Passport Expiry", {})} />
+      <input type="text" placeholder="Nationality" {...register("Nationality", {required: true})} />
+      <select {...register("Gender", { required: true })}>
+        <option value="Male">Male</option>
+        <option value="Female">Female</option>
+      </select>
+      <input type="datetime" placeholder="Date of Birth" {...register("Date of Birth", {required: true})} />
+      <input type="submit" className="bg-red-500" />
+  </form>
   );
 }
