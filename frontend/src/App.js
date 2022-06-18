93fd22141d1355e8d51ceef6dc8b7e1dcb390508
Jolow <jolow999@gmail.com>
2022-06-18 08:59:50 +0800

    Added Login with phone number page, with basic conditional rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Landing from "./pages/Landing.js";
 import Details from "./pages/Details.js";
 import Passport from "./pages/Passport.js";
 import Review from "./pages/Review.js";
+import Login from "./pages/Login.js";
 
 export default function App() {
+  const loggedIn = false;
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={loggedIn? <Landing/> : <Login/>}/>
         <Route path="/" element={<Landing />} />
         <Route path="/details" element={<Details />} />
         <Route path="/passport" element={<Passport />} />
