23240d20a6fe393f23e225c794107096db912918
Jarron-Ng <jarron.ng@gmail.com>
2022-06-20 23:00:54 +0800

    Button still has issue with uploading, using <input> tag for now in details.js

--- backend/customer/config/routes.rb
+++ backend/customer/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+   # this route is needed if not will have 500 internal server error, undefined method 'user_url' for #<Api::V1::UsersController:0x....>
+   post 'api/v1/users' => 'users#create', :as => 'user'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
