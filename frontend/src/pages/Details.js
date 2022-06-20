c09cec6ac0dd98f28b9cc06fef038352fdbc2589
Jarron-Ng <jarron.ng@gmail.com>
2022-06-20 17:50:46 +0800

    get and post done

--- frontend/src/pages/Details.js
+++ frontend/src/pages/Details.js
@@ -1,8 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { API_URL } from "../utilities/constants";
+import axios from "axios";
 
 export default function Details() {
 	const navigate = useNavigate();
+	// const [info, setInfo] = useState({
+		
+	// });
+
 	const {
 		register,
 		handleSubmit,
@@ -10,13 +16,29 @@ export default function Details() {
 	} = useForm();
 	const onSubmit = (data) => {
 		console.log(data);
+
+		/* PROBLEM: active record not properly updating fields
+			TODO: fix this in backend, find out how to create new rows with given in the form below
+			Getting NoMethodError in Api::V1::UserController#create
+		 */
+
+		const test = {
+			title: "Mr",
+			display_name: "John Doe",
+			phone_number: "1234567890",
+			email: "123@gmail.com"
+		};
+
+		axios.post(API_URL, data, { headers: { 'Content-Type': 'application/json' }})
+		.then(response => {console.log(response.data)})
+        .catch(error => {console.log(error.response.data)});
 		navigate("/passport");
 	};
 	console.log(errors);
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<div>
-				<select {...register("Title", { required: true })}>
+				<select {...register("title", { required: true })}>
 					<option value="Mr">Mr</option>
 					<option value="Mrs">Mrs</option>
 					<option value="Ms">Ms</option>
@@ -26,7 +48,7 @@ export default function Details() {
 				<input
 					type="text"
 					placeholder="Last / Display Name"
-					{...register("Last / Display Name", { required: true })}
+					{...register("display_name", { required: true })}
 				/>
 
 				<h3>
@@ -39,13 +61,13 @@ export default function Details() {
 				className="w-full"
 				type="number"
 				placeholder="Phone Number"
-				{...register("Phone Number", { required: true })}
+				{...register("phone_number", { required: true })}
 			/>
 			<input
 				className="w-full"
 				type="email"
 				placeholder="Email Address (Optional)"
-				{...register("Email Address (Optional)", {})}
+				{...register("email", {})}
 			/>
 			<input
 				className="w-full bg-red-500 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
--- frontend/src/utilities/constants.js
+++ frontend/src/utilities/constants.js
@@ -1,3 +1,5 @@
 export const API_URL = "http://localhost:3000/api/v1/users"; //customer data api (for now)
 
+// "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/users"
+
 export default { API_URL };
