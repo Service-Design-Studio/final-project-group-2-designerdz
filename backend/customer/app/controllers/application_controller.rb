05a1a2b54ac6b17e1860ccaacf6bd47dc30e584e
Jarron-Ng <jarron.ng@gmail.com>
2022-06-22 01:39:05 +0800

    added to allow unique phone number to post to database

--- backend/customer/app/controllers/application_controller.rb
+++ backend/customer/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    # use a before_action to do a post/patch request to database
+    # before_action :check_new_user
+
+    # private
+
+    # def check_new_user
+    #     if User.exists?(phone_number: params[:phone_number])
 end
