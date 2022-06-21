a5497def85ee15770e5654128ff3c9f640afe6c9
Jarron-Ng <jarron.ng@gmail.com>
2022-06-21 17:51:33 +0800

    newly added  db fields and phone number query

--- backend/customer/config/routes.rb
+++ backend/customer/config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   namespace :api do #domain/api/
     namespace :v1 do #domain/api/v1
       # resource routing, declare common routes e.g. get,post,put,delete
-      resources :users #domain/api/v1/users
+      resources :users #domain/api/v1/users 
+    
     end 
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # this route is needed if not will have 500 internal server error, undefined method 'user_url' for #<Api::V1::UsersController:0x....>
   post 'api/v1/users' => 'users#create', :as => 'user'
+  get  'api/v1/profile/:phone_number(.:format)', to: 'api/v1/users#show'
 
   # Defines the root path route ("/")
   # root "articles#index"
