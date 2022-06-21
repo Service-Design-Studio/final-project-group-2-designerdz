ce32f203c1f27360dcc121d1452130442f7cac42
Jarron-Ng <jarron.ng@gmail.com>
2022-06-21 21:35:35 +0800

    Added requirement fields for post

--- backend/customer/app/models/user.rb
+++ backend/customer/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+# use to define an accepted array of inputs to return for details.js
+# another method to define an accepted array of inputs to return for passport.js
+# make sure phone number is unique when doing a post request
+# use phone number as primary key instead
+
 end
