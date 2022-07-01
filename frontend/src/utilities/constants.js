9d5b41447d550ff4a847e4d342a391d41aa3e158
Jarron-Ng <jarron.ng@gmail.com>
2022-07-01 14:57:08 +0800

    merged with main

--- frontend/src/utilities/constants.js
+++ frontend/src/utilities/constants.js
@@ -1,7 +1,9 @@
 export const USER_URL =
-  "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/users"; //customer data api (for now)
+  "http://127.0.0.1:3000/api/v1/users/";
+  //"https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/users"; //customer data api (for now)
 
 export const PATCH_USER_URL =
-  "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/profile/"; //update API to update user details, concat phone number at the end
+  "http://127.0.0.1:3000/api/v1/profile/";
+  //"https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/profile/"; //update API to update user details, concat phone number at the end
 
 export default { USER_URL, PATCH_USER_URL };
