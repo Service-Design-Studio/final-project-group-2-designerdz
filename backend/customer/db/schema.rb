c4ed293525539616e9e7b4e3a3d6584c0615b61a
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-07-02 00:12:25 +0800

    fixed changecolumn name

--- backend/customer/db/schema.rb
+++ backend/customer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_17_162805) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_155524) do
   create_table "users", force: :cascade do |t|
     t.string "display_name"
     t.string "title"
