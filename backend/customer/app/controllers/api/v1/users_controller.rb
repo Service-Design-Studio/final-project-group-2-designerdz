033d1920d0ed2524ce91602d525041bafcd8d94a
Jarron-Ng <jarron.ng@gmail.com>
2022-07-11 17:46:03 +0800

    Changed uri to /parent?=id

--- backend/customer/app/controllers/api/v1/users_controller.rb
+++ backend/customer/app/controllers/api/v1/users_controller.rb
@@ -23,16 +23,19 @@ class Api::V1::UsersController < ApplicationController
   def create
     # checks if user exists when client press 'Next', since frontend can't differentiate whether it's a POST or PATCH
     # the params[:id] is to handle case when user edits their phone number
-    #if (User.exists?(phone_number: params[:phone_number]) || User.exists?(id: params[:id]))
+    if (User.exists?(phone_number: params[:phone_number]) || User.exists?(id: params[:id]))
     #   @user = User.where(id: params['id'])
     #   @user.update(user_params)
     # creates a new record if it dosen't exist  
+      flash[:error] = "Error"
+    else
       @user = User.new(user_params)
       if @user.save
         render json: @user, status: :created, location: @user
       else
         render json: @user.errors, status: :unprocessable_entity
       end 
+    end 
   end 
 
   # PATCH/PUT /api/v1/users/:id ## when users click on next buttons
