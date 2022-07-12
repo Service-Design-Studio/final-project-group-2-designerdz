0657c4a401b30caf6e121e46bcec6f5f016aca7e
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-07-12 15:25:50 +0800

    update fixme checkbox autofill

--- frontend/features/step_definitions/checkbox_autofill_steps.js
+++ frontend/features/step_definitions/checkbox_autofill_steps.js
@@ -18,6 +18,7 @@ const { initDriver } = require("../support/driverUtil");
 const { expect, assert, AssertionError } = require("chai");
 const { setDefaultTimeout } = require("@cucumber/cucumber");
 const pactum = require("pactum");
+import { deleteAllData } from "../../services/axiosRequests.js";
 
 let spec = pactum.spec();
 
@@ -36,11 +37,13 @@ After(async function () {
   await driver.quit();
 });
 
-Given("that I have saved my details", async function() {
+Given("that I have saved my details", async function () {
   await driver.get(base_url + "details");
   await driver.sleep(1000);
 
-  const parentName = await driver.findElement(By.className("parent_display_name"));
+  const parentName = await driver.findElement(
+    By.className("parent_display_name")
+  );
   parentName.sendKeys("Sally Abbot");
   const parentEmail = await driver.findElement(By.className("parent_email"));
   parentEmail.sendKeys("sally_abbot@gmail.com");
@@ -49,6 +52,7 @@ Given("that I have saved my details", async function() {
 
   const nextButton = await driver.findElement(By.className("next"));
 
+  deleteAllData();
   // FIXME: Having issues with the alert
   // nextButton.click();
 
@@ -56,9 +60,9 @@ Given("that I have saved my details", async function() {
 
   // var actual_url = await driver.getCurrentUrl();
   // assert.equal(actual_url, base_url + "family");
-})
+});
 
-Given("I add a new child", async function() {
+Given("I add a new child", async function () {
   // TODO: Remove once the top works
   await driver.get(base_url + "family");
   await driver.sleep(1000);
@@ -71,20 +75,20 @@ Given("I add a new child", async function() {
 
   var actual_url = await driver.getCurrentUrl();
   assert.equal(actual_url, base_url + "child");
-})
+});
 
-When("I check the autofill checkbox", async function() {
+When("I check the autofill checkbox", async function () {
   const autofillCheckbox = await driver.findElement(By.className("autofill"));
-  
-  if(!autofillCheckbox.isSelected()) {
+
+  if (!autofillCheckbox.isSelected()) {
     autofillCheckbox.click();
   }
 
   var checked = await autofillCheckbox.isSelected();
   assert.equal(checked, true);
-})
+});
 
-Then("I should see my child details autofilled", async function(){
+Then("I should see my child details autofilled", async function () {
   // FIXME: To show the number?
   const childNumber = await driver.findElement(By.className("child_number"));
   var childNumberValue = await childNumber.getAttribute("value");
@@ -92,26 +96,26 @@ Then("I should see my child details autofilled", async function(){
 
   var childEmail = driver.findElement(By.className("child_email"));
   var childEmailValue = await childEmail.getAttribute("value");
-  assert.equal(childEmailValue, "sally_abbot@gmail.com");  
-})
+  assert.equal(childEmailValue, "sally_abbot@gmail.com");
+});
 
-When("I uncheck the autofill checkbox", async function() {	
+When("I uncheck the autofill checkbox", async function () {
   const autofillCheckbox = await driver.findElement(By.className("autofill"));
 
-  if(autofillCheckbox.isSelected()) {
+  if (autofillCheckbox.isSelected()) {
     autofillCheckbox.click();
   }
 
   var checked = await autofillCheckbox.isSelected();
   assert.equal(checked, false);
-})
+});
 
-Then("I should see my child details as empty", async function(){
+Then("I should see my child details as empty", async function () {
   const childNumber = await driver.findElement(By.className("child_number"));
   var childNumberValue = await childNumber.getAttribute("value");
   assert.equal(childNumberValue, "");
 
   var childEmail = driver.findElement(By.className("child_email"));
   var childEmailValue = await childEmail.getAttribute("value");
-  assert.equal(childEmailValue, "");  
-})
+  assert.equal(childEmailValue, "");
+});
