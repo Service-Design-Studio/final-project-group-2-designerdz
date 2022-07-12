13aeab69dc937fe761cbe68e64b252c35ab2e005
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-07-12 14:57:34 +0800

    change to constants.js

--- frontend/src/utilities/constants.js
+++ frontend/src/utilities/constants.js
@@ -1,17 +1,21 @@
-export const USER_API =
-  // "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/users"
-  "http://localhost:3000/api/v1/users";
+const URL =
+  // "http://localhost:3000";
+  "https://dbs-service-qz6esvmepq-as.a.run.app";
 
-export const CHILD_API =
-  // "https://dbs-service-qz6esvmepq-as.a.run.app/children"
-  "http://localhost:3000/children";
+export const USER_API = URL.concat("/api/v1/users");
+// // "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/users"
+// "http://localhost:3000/api/v1/users";
 
-export const CHILDREN_API =
-  // "https://dbs-service-qz6esvmepq-as.a.run.app/parent"
-  "http://localhost:3000/parent";
+export const CHILD_API = URL.concat("/children");
+// // "https://dbs-service-qz6esvmepq-as.a.run.app/children"
+// ("http://localhost:3000/children");
 
-export const DELETE_ALL_API =
-  // "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/profile/delete"
-  "http://localhost:3000/api/v1/profile/delete";
+export const CHILDREN_API = URL.concat("/parent");
+// // "https://dbs-service-qz6esvmepq-as.a.run.app/parent"
+// "http://localhost:3000/parent";
+
+export const DELETE_ALL_API = URL.concat("/api/v1/profile/delete");
+// // "https://dbs-service-qz6esvmepq-as.a.run.app/api/v1/profile/delete"
+// "http://localhost:3000/api/v1/profile/delete";
 
 export default { USER_API, CHILD_API, CHILDREN_API, DELETE_ALL_API };
