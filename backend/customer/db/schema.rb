865d93cf67553e72db9076db68dea53600166543
Jarron-Ng <jarron.ng@gmail.com>
2022-07-18 17:29:29 +0800

    added url column

--- backend/customer/db/schema.rb
+++ backend/customer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_104045) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_092804) do
   create_table "children", force: :cascade do |t|
     t.integer "user_id"
     t.string "display_name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_104045) do
     t.string "dob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   add_foreign_key "children", "users"
