94199c2dba9a7dfd4ab625a5ed4e8196d23feaaf
chuanshaof <chuanshaof@gmail.com>
2022-07-20 21:59:55 +0800

    form state

--- frontend/features/form_state.feature
+++ frontend/features/form_state.feature
@@ -4,13 +4,22 @@ Feature: Check API Response
         When I receive a response within 0.5 seconds
         Then response should have a status 200
 
-    # Scenario: Navigating back and forth and saving of forms
+    Scenario: Navigating back and seeing forms saved
+        Given that I have filled up <previous> page and have navigated to <page>
+        When I navigate back to the <previous> page
+        Then the fields I have filled up in <previous> page should remain
+
+        Examples:
+        | previous   | page       |
+        | "details"  | "passport" |
+        | "passport" | "review"   |
+
+    # Scenario: Navigating forward and seeing forms saved
     #     Given that I have filled up <previous> page and have navigated to <page>
     #     When I click on the back button
-    #     Then the <fields> I have filled up in <previous> page should remain
+    #     Then the fields I have filled up in <previous> page should remain
 
     #     Examples:
     #     | previous   | page       |
-    #     | ""         | "signup"   |
     #     | "details"  | "passport" |
     #     | "passport" | "review"   |
--- frontend/features/step_definitions/form_state_steps.js
+++ frontend/features/step_definitions/form_state_steps.js
@@ -27,8 +27,7 @@ let baseUrl = "http://localhost:3001/";
 
 setDefaultTimeout(60 * 1000);
 
-let parent_number = Math.floor(Math.random() * 10000);
-let child_number = Math.floor(Math.random() * 10000);
+let user_number;
 
 let driver;
 
@@ -39,6 +38,8 @@ Before(async function () {
   await driver.executeScript(function () {
     localStorage.clear();
   });
+
+  user_number = Math.floor(Math.random() * 10000);
   spec = pactum.spec();
 });
 
@@ -60,4 +61,55 @@ When('I receive a response within 0.5 seconds', async function () {
 
 Then('response should have a status {int}', async function (code) {
   await spec.response().should.have.status(code);
+});
+
+Given("that I have filled up {string} page and have navigated to {string}", async function(previous, page) {
+  await driver.get(baseUrl + "signup");
+  await driver.sleep(500);
+
+  // Navigate into single user details
+  await driver.findElement(By.className("next")).click();
+  await driver.sleep(500);
+  assert.equal(await driver.getCurrentUrl(), baseUrl + "details");
+
+  await driver.findElement(By.className("display_name")).sendKeys("Yi Ma");
+  await driver.findElement(By.className("phone_number")).sendKeys(user_number);
+  await driver.findElement(By.className("email")).sendKeys("dayima@gmail.com");
+
+  await driver.findElement(By.className("next")).click();
+  await driver.sleep(500);
+
+  if (page == "previous") {
+    await driver.findElement(By.className("full_name")).sendKeys("Da Yi Ma");
+    await driver.findElement(By.className("passport_number")).sendKeys("E32136512");
+    await driver.findElement(By.className("nationality")).sendKeys("China");
+    await driver.findElement(By.className("female")).click();
+    await driver.sleep(500);
+
+    // Move to review page
+    await driver.findElement(By.className("next")).click();
+    await driver.sleep(500); 
+  }
+
+  assert.equal(await driver.getCurrentUrl(), baseUrl + page);
+});
+
+When("I navigate back to the {string} page", async function(previous) {
+  const registration_button = await driver.findElement(By.className("back"));
+  await registration_button.click();
+  await driver.sleep(500);
+
+  assert.equal(await driver.getCurrentUrl(), baseUrl + previous);
+});
+
+Then("the fields I have filled up in {string} page should remain", async function(previous) {
+  if (previous == "details") {
+    assert.equal(await driver.findElement(By.className("display_name")).getAttribute("value"), "Yi Ma");
+    assert.equal(await driver.findElement(By.className("phone_number")).getAttribute("value"), user_number);
+    assert.equal(await driver.findElement(By.className("email")).getAttribute("value"), "dayima@gmail.com");
+  } else if (previous == "passport") {
+    assert.equal(await driver.findElement(By.className("full_name")).getAttribute("value"), "Da Yi Ma");
+    assert.equal(await driver.findElement(By.className("passport_number")).getAttribute("value"), "E32136512");
+    assert.equal(await driver.findElement(By.className("nationalty")).getAttribute("value"), "China");
+  }
 });
