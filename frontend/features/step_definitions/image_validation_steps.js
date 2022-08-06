040fd1cbf1d24800aa7b2c3fc8668b471bb8d2e8
stjianqing <stjianqing@gmail.com>
2022-08-07 02:43:56 +0800

    fixed image_validation.feature

--- frontend/features/step_definitions/image_validation_steps.js
+++ frontend/features/step_definitions/image_validation_steps.js
@@ -3,13 +3,37 @@ const { By } = require("selenium-webdriver");
 const { expect, assert } = require("chai");
 
 Given("I am on the passport page", async function() {
-    
-})
+  await driver.get(baseUrl);
+  await driver.sleep(2000);
 
-When("I upload a bad {string}", async function(image) {
-})
+  const notACustomerYetButton = await driver.findElement(
+    By.className("bg-red-500")
+  );
+  notACustomerYetButton.click();
+  await driver.sleep(1000);
 
-Then("I should see {error}", async function(error) {
-    const errorName = await driver.findElement(By.id("upload-error")).getText();
+  const goToDetailsButton = await driver.findElement(By.className("next"));
+  goToDetailsButton.click();
+  await driver.sleep(1000);
+
+  await driver.findElement(By.className("display_name")).sendKeys("John");
+  await driver.findElement(By.className("phone_number")).sendKeys(parentNumber);
+
+  const goToPassportButton = await driver.findElement(By.className("next"));
+  goToPassportButton.click();
+  await driver.sleep(5000);
+  expect(await driver.getCurrentUrl()).to.equal(baseUrl + "passport");
+});
+
+When("I upload a bad {string}", async function (image) {
+  const uploadButton = await driver.findElement(By.className("btn_upload"));
+  var path = process.cwd()+ '/features' + '/resources/'+ image;
+  uploadButton.sendKeys(path);
+  await driver.sleep(5000);
+
+});
+
+Then("I should observe {string}", async function (error) {
+    const errorName = await driver.findElement(By.className("text-red-500")).getText();
     assert.equal(errorName, error);
-})
+});
