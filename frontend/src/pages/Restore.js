73d2fd82aad9387d630975186bcb2b31039a9e55
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-08-10 17:16:05 +0800

    removed required character restore.js

--- frontend/src/pages/Restore.js
+++ frontend/src/pages/Restore.js
@@ -52,7 +52,7 @@ export default function Restore() {
         className="mobile_no border border-solid border-red-400 rounded w-max mx-auto p-2"
         type="number"
         placeholder="Mobile Number"
-        {...register("phone_number", { required: true, maxLength: 8 })}
+        {...register("phone_number")}
       />
       <button
         className="continue_btn bg-red-500 text-white rounded px-8 py-2 mx-auto my-4 w-[40vw] text-xl "
